43b1aff71fd50b7d45bb9d9ee17d239219793f8e
Florian Roser <Florian.Roser@sick.de>
2019-08-26 18:38:01 +0200

    'Update'

--- RGBtoHSVconversion/scripts/RGBtoHSVconversion.lua
+++ RGBtoHSVconversion/scripts/RGBtoHSVconversion.lua
@@ -19,7 +19,6 @@
   Tutorial "Algorithms - Color".
 
 ------------------------------------------------------------------------------]]
-
 --Start of Global Scope---------------------------------------------------------
 
 -- Delay in ms between visualization steps for demonstration purpose
@@ -27,12 +26,12 @@ local DELAY = 2000
 
 -- Create viewer
 local viewer = View.create()
-viewer:setID('viewer2D')
 
 -- Setting up graphical overlay attributes
 local textDecoration = View.TextDecoration.create()
 textDecoration:setPosition(20, 50)
 textDecoration:setSize(40)
+textDecoration:setColor(0, 220, 0)
 
 local decoration = View.ShapeDecoration.create()
 decoration:setLineColor(0, 230, 0) -- Green
@@ -44,21 +43,19 @@ decoration:setLineColor(0, 230, 0) -- Green
 -- Viewing image with text label
 --@show(img:Image, name:string)
 local function show(img, name)
-  viewer:addImage(img)
-  viewer:addText(name, textDecoration)
+  viewer:clear()
+  local imid = viewer:addImage(img)
+  viewer:addText(name, textDecoration, nil, imid)
   viewer:present()
   Script.sleep(DELAY) -- for demonstration purpose only
 end
 
 local function main()
   local img = Image.load('resources/RGBtoHSVconversion.bmp')
-  viewer:clear()
-  viewer:addImage(img)
-  viewer:present()
-  Script.sleep(DELAY)
+  show(img, 'Input image')
 
   -- Convert to HSV color space (Hue, Saturation, Value)
-  local H, S, V = Image.toHSV(img)
+  local H, S, V = img:toHSV()
   show(H, 'Hue') -- View image with text label and delay
   show(S, 'Saturation')
   show(V, 'Value')
